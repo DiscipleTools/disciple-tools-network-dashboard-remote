9d16f5f13bd3dc3929d0c378ff38ae34ea52ff18
Chris Chasm <chris@chasm.solutions>
2020-09-23 06:00:50 -0600

    Added profile endpoint

--- includes/rest-api.php
+++ includes/rest-api.php
@@ -42,12 +42,60 @@ class DT_ND_Remote_Endpoints
                 ],
             ]
         );
+
+        register_rest_route(
+            'dt-public/v1', '/network_dashboard/profile', [
+                'methods'  => 'POST',
+                'callback' => [ $this, 'profile' ],
+            ]
+        );
     }
 
     public function rest_log( WP_REST_Request $request ) {
         /* VERIFY REQUIRED PARAMS AND SANITIZE */
         $params = $request->get_params();
         return Network_Dashboard_Remote_Log::log( $params );
+    }
+
+    public function profile( WP_REST_Request $request ) {
+        $params = $this->process_token( $request );
+        if ( is_wp_error( $params ) ) {
+            return [
+                'status' => 'FAIL',
+                'error' => $params,
+            ];
+        }
+        return dt_network_site_profile();
+    }
+    /**
+     * Process the standard security checks on an api request to network endpoints.
+     * @param \WP_REST_Request $request
+     *
+     * @return array|\WP_Error
+     */
+    public function process_token( WP_REST_Request $request ) {
+
+        $params = $request->get_params();
+
+        // required token parameter challenge
+        if ( ! isset( $params['transfer_token'] ) ) {
+            return new WP_Error( __METHOD__, 'Missing parameters.' );
+        }
+
+        $valid_token = Site_Link_System::verify_transfer_token( $params['transfer_token'] );
+
+        // required valid token challenge
+        if ( ! $valid_token ) {
+            dt_write_log( $valid_token );
+            return new WP_Error( __METHOD__, 'Invalid transfer token' );
+        }
+
+        // Add post id for site to site link
+        $decrypted_key = Site_Link_System::decrypt_transfer_token( $params['transfer_token'] );
+        $keys = Site_Link_System::get_site_keys();
+        $params['site_post_id'] = $keys[$decrypted_key]['post_id'];
+
+        return $params;
     }
 }
 DT_ND_Remote_Endpoints::instance();
