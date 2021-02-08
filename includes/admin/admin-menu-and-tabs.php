f3511dff011ad23c12b32ac5d0d98a870df68fc7
corsacca <corsacca@gmail.com>
2021-02-08 17:50:36 +0100

    Remove duplicate menu declaration: Extensions (DT)

--- includes/admin/admin-menu-and-tabs.php
+++ includes/admin/admin-menu-and-tabs.php
@@ -58,7 +58,6 @@ class DT_ND_Remote_Menu {
      * @since 0.1
      */
     public function register_menu() {
-        add_menu_page( __( 'Extensions (DT)', 'disciple_tools' ), __( 'Extensions (DT)', 'disciple_tools' ), 'manage_dt', 'dt_extensions', [ $this, 'extensions_menu' ], 'dashicons-admin-generic', 59 );
         add_submenu_page( 'dt_extensions', __( 'Network Dashboard Remote', 'dt_nd_remote' ), __( 'Network Dashboard Remote', 'dt_nd_remote' ), 'manage_dt', $this->token, [ $this, 'content' ] );
     }
 
